32bc206e3f306e198fb3753f9dc65560f604483b
brooktewabe <brooktewabe00@gmail.com>
2024-05-17 10:27:47 +0300

    'items-and-orders'

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,1 @@
 from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.getData),
-    path('detail/<pk>/', views.getDetail),
-    path('add/', views.addItem),
-    path('update/<pk>/', views.updateItem),
-    path('delete/<pk>/', views.deleteItem),
-]
